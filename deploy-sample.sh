317ac0974830aadf1d82082a20af08882e645969
codedrinker <gwangchunlei@gmail.com>
2016-12-18 00:11:56 +0800

    fix

--- deploy-sample.sh
+++ deploy-sample.sh
@@ -1,16 +1,18 @@
 sh git-pull.sh master ../git-continue-learning
 status=$?
 if [ $status -eq 0 ]; then 
-	echo "exec successfully."
-	echo "start mvn build"
-	pwd
-	sh mvn-package.sh shell-script vagrant
+	echo "Exec successfully."
+	echo "Start mvn build"
+	sh mvn-package.sh git-continue-learning vagrant ../git-continue-learning
+	mvn_status=$?
+	if [ $mvn_status -eq 0 ]; then
+		echo "Maven build successfully."
+	else
+		echo "Maven build failed."
+		exit 1
+	fi
 elif [ $status -eq 2 ]; then 
-	echo "no need update code."
-	echo "start mvn build"
-	pwd
-	sh mvn-package.sh shell-script vagrant
-	echo $?
+	echo "Don't need mvn package."
 else
 	echo "something wrong."
 	exit 1
