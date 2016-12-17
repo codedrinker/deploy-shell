c5e47ecaf3738f2158f64897a6ffe10ed8407c09
codedrinker <gwangchunlei@gmail.com>
2016-12-17 23:59:56 +0800

    add mvn package

--- deploy-sample.sh
+++ deploy-sample.sh
@@ -2,8 +2,15 @@ sh git-pull.sh master ../git-continue-learning
 status=$?
 if [ $status -eq 0 ]; then 
 	echo "exec successfully."
+	echo "start mvn build"
+	pwd
+	sh mvn-package.sh shell-script vagrant
 elif [ $status -eq 2 ]; then 
 	echo "no need update code."
+	echo "start mvn build"
+	pwd
+	sh mvn-package.sh shell-script vagrant
+	echo $?
 else
 	echo "something wrong."
 	exit 1
---
+++ mvn-package.sh
@@ -0,0 +1,34 @@
+MAVEN_PROJECT_HOME="."
+MAVEN_PROJECT_NAME=""
+MAVEN_PROJECT_PROFILE=""
+if [ $# -lt 2 ]; then
+	echo "Usage: Please given more than 1 param."
+	echo "sh mvn-package.sh shell-script sandbox"
+	exit 1
+fi
+if [ $# == 2 ]; then
+	MAVEN_PROJECT_NAME=$1
+	MAVEN_PROJECT_PROFILE=$2
+fi
+
+if [ $# == 3 ]; then
+	MAVEN_PROJECT_NAME=$1
+	MAVEN_PROJECT_PROFILE=$2
+	MAVEN_PROJECT_HOME=$3
+fi
+
+eval "cd $MAVEN_PROJECT_HOME"
+echo "Current directory"
+pwd
+
+echo "Start package maven module $MAVEN_PROJECT_NAME, profile $MAVEN_PROJECT_PROFILE."
+echo "mvn clean -U -DskipTests package -pl :$MAVEN_PROJECT_NAME -am -P $MAVEN_PROJECT_PROFILE"
+eval "mvn clean -U -DskipTests package -pl :$MAVEN_PROJECT_NAME -am -P $MAVEN_PROJECT_PROFILE"
+STATUS=$?
+if [ $STATUS -eq 0 ]
+then
+	echo "Maven package Successful."
+else
+	echo "Maven package Failed."
+	exit 1
+fi
