002a33e7d69404fa7014453bd3cb18a409240ebf
codedrinker <gwangchunlei@gmail.com>
2016-12-18 22:30:10 +0800

    add some try catch

--- deploy-sample.sh
+++ deploy-sample.sh
@@ -1,34 +1,24 @@
 PROJECT_HOME="/Users/codedrinker/Code/shell-script-web-sample"
-sh git-pull.sh master $PROJECT_HOME
-status=$?
-if [ $status -eq 0 ]; then 
-	echo "Exec successfully."
-	echo "Start mvn build"
-	sh mvn-package.sh shell-script-web-sample vagrant $PROJECT_HOME
-	mvn_status=$?
-	if [ $mvn_status -eq 0 ]; then
-		echo "Maven package successfully."
-		sh migrate.sh vagrant $PROJECT_HOME
-		migrate_status=$?
-		if [ $migrate_status -eq 0 ]; then
-			echo "Migrate successfully."
-			sh tomcat-deploy.sh /Users/codedrinker/App/apache-tomcat-7.0.67 /Users/codedrinker/Code/shell-script-web-sample/target/shell-script-web-sample-0.0.1.war ROOT
-			tomcat_deploy_status=$?
-			if [ $tomcat_deploy_status -eq 0 ]; then
-				echo "Deploy successfully."
-			else
-				echo "Deploy failed."
-			fi  
-		else
-			echo "Migrate failed."
-			exit 1
-		fi
-	else
-		echo "Maven build failed."
+file="$HOME/shell-script.pid"
+
+function clean_up {
+	rm $file
+	exit 1
+}
+
+function check_process {
+	if [ -f "$file" ]
+	then
+		echo "redeploy script is already running!! on pid"
+		more $file
 		exit 1
+	else
+		echo $$ > "$file"
+		echo "Start deploy"	
 	fi
-elif [ $status -eq 2 ]; then 
-	echo "Don't need mvn package."
+}
+
+function deploy {
 	sh tomcat-deploy.sh /Users/codedrinker/App/apache-tomcat-7.0.67 /Users/codedrinker/Code/shell-script-web-sample/target/shell-script-web-sample-0.0.1.war ROOT
 	tomcat_deploy_status=$?
 	if [ $tomcat_deploy_status -eq 0 ]; then
@@ -36,7 +26,48 @@ elif [ $status -eq 2 ]; then
 	else
 		echo "Deploy failed."
 	fi  
-else
-	echo "something wrong."
-	exit 1
-fi
+}
+
+function process {
+	sh git-pull.sh master $PROJECT_HOME
+	status=$?
+	if [ $status -eq 0 ]; then 
+		echo "Exec successfully."
+		echo "Start mvn build"
+		sh mvn-package.sh shell-script-web-sample vagrant $PROJECT_HOME
+		mvn_status=$?
+		if [ $mvn_status -eq 0 ]; then
+			echo "Maven package successfully."
+			sh migrate.sh vagrant $PROJECT_HOME
+			migrate_status=$?
+			if [ $migrate_status -eq 0 ]; then
+				echo "Migrate successfully."
+				deploy
+			else
+				echo "Migrate failed."
+				exit 1
+			fi
+		else
+			echo "Maven build failed."
+			exit 1
+		fi
+	elif [ $status -eq 2 ]; then 
+		echo "Don't need mvn package."
+		deploy
+	else
+		echo "something wrong."
+		exit 1
+	fi
+}
+
+# delete temp file when exit unexcepted
+trap clean_up SIGHUP SIGINT SIGTERM
+
+# check the process is already running
+check_process
+
+# main process
+process
+
+rm $file
+
